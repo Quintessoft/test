f94742a916aff4df9b5366801b9d549d65eeb0fc
gchak789 <gchak789@yahoo.co.in>
2013-08-08 10:02:50 +0530

    First Commit

---
+++ src/com/example/testapplication/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.testapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	public static void main() {
+		
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.id.layout2);
+	}
+	
+
+}
